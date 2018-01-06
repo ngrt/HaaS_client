0ec2c038d9fe5b22ba352d45b3d5a18f340bdf71
Noufel Gouirhate <noufel.gouirhate@gmail.com>
2018-01-06 14:54:55 +0100

    adding template and packages

--- HaaS_client.py
+++ HaaS_client.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request, redirect, url_for, flash, session
+import requests
+import json
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisissecret'
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    #r = requests.get('http://127.0.0.1:5000/article')
+    #articles = json.loads(r.text)['articles']
+
+    return render_template('index.html', articles=[])
+
+@app.route('/login')
+def login():
+    #r = requests.get('http://127.0.0.1:5000/article')
+    #articles = json.loads(r.text)['articles']
+
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
