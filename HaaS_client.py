442659b8d98a287307143eb030a3fcc3f127d071
Noufel Gouirhate <noufel.gouirhate@gmail.com>
2018-01-06 15:14:04 +0100

    login route ok

--- HaaS_client.py
+++ HaaS_client.py
@@ -1,9 +1,29 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for, flash, session
 import requests
 import json
+from wtforms import *
+from wtforms.validators import *
+from flask_wtf import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisissecret'
+
+class CreateArticleForm(Form):
+    title = StringField('Title', [InputRequired()])
+    body = TextAreaField('Text', [InputRequired()])
+
+class RegisterUserForm(Form):
+    email = StringField('Email', [InputRequired()])
+    name = StringField('Name', [InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired(),
+                                                     EqualTo('password_confirmation', message='Passwords must match'),
+                                                     Length(min=5)])
+    password_confirmation = PasswordField('Password confirmation', validators=[InputRequired()])
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+
 
 @app.route('/')
 def index():
@@ -14,10 +34,27 @@ def index():
 
 @app.route('/login')
 def login():
-    #r = requests.get('http://127.0.0.1:5000/article')
-    #articles = json.loads(r.text)['articles']
+    loginForm = LoginForm()
+    if loginForm.validate_on_submit():
+        r = requests.get('http://127.0.0.1:5000/login', auth=HTTPBasicAuth(loginForm.email.data, loginForm.password.data))
 
-    return render_template('login.html')
+        if json.loads(r.text)['token']:
+            session['token'] = json.loads(r.text)['token']
+            flash("You are logged in")
+            return redirect(url_for('index'))
+
+    return render_template('login.html', loginForm=loginForm)
+
+@app.route('/logout', methods=['GET', 'POST'])
+#@require_token
+def logout(current_user):
+    session.pop('token', None)
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    registerUserForm = RegisterUserForm()
+    return render_template('register.html', registerUserForm=registerUserForm)
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
